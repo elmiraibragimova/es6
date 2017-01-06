809cfa66f4a0563279f19bc45a04e9d124d99a66
Elmira Ibragimova <elmira.ibragimova28@gmail.com>
2017-01-07 01:58:35 +0300

    start commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  entry: './src/main.js',
+
+  output: {
+    path: __dirname + './build',
+    filename: './js/main.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      include: __dirname + './src',
+      loader: 'babel?preset[]=es2015'
+    }]
+  },
+
+  devServer: {
+    contentBase: __dirname + '/build',
+    hot: true
+  }
+}
